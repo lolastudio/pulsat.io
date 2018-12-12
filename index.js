b02d90062e5fff10291467372f14119b3baf038e
Roque Francisco <roquefsd@gmail.com>
2018-12-12 18:11:24 -0200

    v0.1.4, build

--- index.js
+++ index.js
@@ -1,224 +1,262 @@
-const request = require('request')
-const uuidv4 = require('uuid/v4')
-const os = require('os')
+'use strict';
 
-class Pulsatio {
-    constructor(options = {}) {
-        let defaults = {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var request = require('request');
+var uuidv4 = require('uuid/v4');
+var os = require('os');
+
+var Pulsatio = function () {
+    function Pulsatio() {
+        var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+        _classCallCheck(this, Pulsatio);
+
+        var defaults = {
             port: 4200,
             url: 'http://localhost:4200',
             interval: 30 * 1000,
             interval_timeout: 1.1
-        }
+        };
 
-        options = Object.assign(defaults, options)
+        options = Object.assign(defaults, options);
 
         this.ENDPOINTS = {
             register: '/nodes',
             getAllNodes: '/nodes'
-        }
+        };
 
         if (!options.express) {
-            var express = require('express')
-            this.express = express()
+            var express = require('express');
+            this.express = express();
+        } else {
+            this.express = options.express;
         }
-        else {
-            this.express = options.express
-        }
 
-        this.nodes = {}
-        this.options = options
+        this.nodes = {};
+        this.options = options;
 
-        this.registerNewNode = this.registerNewNode.bind(this)
-        this.pulsatio = this.pulsatio.bind(this)
-        this.getNode = this.getNode.bind(this)
-        this.getAllNodes = this.getAllNodes.bind(this)
-        this.sendHeartbeat = this.sendHeartbeat.bind(this)
-        this.connect = this.connect.bind(this)
+        this.registerNewNode = this.registerNewNode.bind(this);
+        this.pulsatio = this.pulsatio.bind(this);
+        this.getNode = this.getNode.bind(this);
+        this.getAllNodes = this.getAllNodes.bind(this);
+        this.sendHeartbeat = this.sendHeartbeat.bind(this);
+        this.connect = this.connect.bind(this);
 
-        this.init()
+        this.init();
     }
 
-    init() {
-        if (this.options.mode === 'server') {
-            this.initServer()
+    _createClass(Pulsatio, [{
+        key: 'init',
+        value: function init() {
+            if (this.options.mode === 'server') {
+                this.initServer();
+            } else {
+                this.initClient();
+            }
         }
-        else {
-            this.initClient()
-        }
-    }
+    }, {
+        key: 'initServer',
+        value: function initServer() {
+            var _this = this;
 
-    initServer() {
-        let address
-        if (this.express.address) {
-            try {
-                address = this.express.address()
-            } catch (err) { }
-        }
-
-        var bodyParser = require('body-parser')
-        this.express.use(bodyParser.json())
-
-        if (!address) {
-            this.express.listen(this.options.port, () => {
-                this.log(`Pulsat.io @ ${this.options.port}`)
-            })
-        }
-
-        this.initServerEndpoints()
-    }
-
-    initServerEndpoints() {
-        this.express.put('/nodes/:id', this.pulsatio)
-        this.express.get('/nodes/:id', this.getNode)
-        this.express.get(this.ENDPOINTS.getAllNodes, this.getAllNodes)
-        this.express.post(this.ENDPOINTS.register, this.registerNewNode)
-        this.express.delete('/nodes/:id', this.deregisterNode)
-    }
-
-    pulsatio(req, res) {
-        let node = this.nodes[req.params.id]
-
-        if (node) {
-            clearTimeout(node.timeout)
-            this.nodes[req.params.id].online = true;
-            this.nodes[req.params.id].lastHeartbeat = new Date()
-            this.nodes[req.params.id].timeout = setTimeout(() => {
-                this.nodes[req.params.id].online = false
-            }, node.interval * this.options.interval_timeout)
-
-            res.sendStatus(200)
-        }
-        else {
-            res.sendStatus(404)
-        }
-    }
-
-    getNode(req, res) {
-        if (!res) {
-            return this.clearNode(this.nodes[req])
-        }
-        else {
-            res.send(this.clearNode(this.nodes[req.params.id]))
-        }
-    }
-
-    getAllNodes(req, res) {
-        if (!res) {
-            return this.clearNode(this.nodes, true)
-        }
-        else {
-            res.send(this.clearNode(this.nodes, true))
-        }
-    }
-
-    registerNewNode(req, res) {
-        let info = req.body
-        info.online = true
-
-        if (info.id && this.nodes[info.id]) {
-            res.send({ info: 'Already registered!' })
-            return
-        }
-        if (!info.id) {
-            info.id = uuidv4()
-        }
-
-        info.registeredAt = new Date()
-        info.lastHeartbeat = new Date()
-        this.nodes[info.id] = Object.assign({}, info)
-
-        res.send(info)
-    }
-
-    deregisterNode(req, res) {
-        if (!res) {
-            this.nodes[req] = null
-            delete this.nodes[req]
-        }
-        else {
-            this.nodes[req.params.id] = null
-            delete this.nodes[req.params.id]
-        }
-    }
-
-    initClient() {
-        this.connect()
-    }
-
-    connect() {
-        if (this.options.url) {
-            let url = this.options.url + this.ENDPOINTS.register
-            let data = {
-                id: this.options.id,
-                ip: ip(),
-                interval: this.options.interval,
-                hostname: os.hostname()
+            var address = void 0;
+            if (this.express.address) {
+                try {
+                    address = this.express.address();
+                } catch (err) {}
             }
 
-            request.post(url, { json: data }, (e, r, body) => {
-                if (body && body.id) {
-                    this.options.id = body.id
+            var bodyParser = require('body-parser');
+            this.express.use(bodyParser.json());
+
+            if (!address) {
+                this.express.listen(this.options.port, function () {
+                    _this.log('Pulsat.io @ ' + _this.options.port);
+                });
+            }
+
+            this.initServerEndpoints();
+        }
+    }, {
+        key: 'initServerEndpoints',
+        value: function initServerEndpoints() {
+            this.express.put('/nodes/:id', this.pulsatio);
+            this.express.get('/nodes/:id', this.getNode);
+            this.express.get(this.ENDPOINTS.getAllNodes, this.getAllNodes);
+            this.express.post(this.ENDPOINTS.register, this.registerNewNode);
+            this.express.delete('/nodes/:id', this.deregisterNode);
+        }
+    }, {
+        key: 'pulsatio',
+        value: function pulsatio(req, res) {
+            var _this2 = this;
+
+            var node = this.nodes[req.params.id];
+
+            if (node) {
+                clearTimeout(node.timeout);
+                this.nodes[req.params.id].online = true;
+                this.nodes[req.params.id].lastHeartbeat = new Date();
+                this.nodes[req.params.id].timeout = setTimeout(function () {
+                    _this2.nodes[req.params.id].online = false;
+                }, node.interval * this.options.interval_timeout);
+
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(404);
+            }
+        }
+    }, {
+        key: 'getNode',
+        value: function getNode(req, res) {
+            if (!res) {
+                return this.clearNode(this.nodes[req]);
+            } else {
+                res.send(this.clearNode(this.nodes[req.params.id]));
+            }
+        }
+    }, {
+        key: 'getAllNodes',
+        value: function getAllNodes(req, res) {
+            if (!res) {
+                return this.clearNode(this.nodes, true);
+            } else {
+                res.send(this.clearNode(this.nodes, true));
+            }
+        }
+    }, {
+        key: 'registerNewNode',
+        value: function registerNewNode(req, res) {
+            var info = req.body;
+            info.online = true;
+
+            if (info.id && this.nodes[info.id]) {
+                res.send({ info: 'Already registered!' });
+                return;
+            }
+            if (!info.id) {
+                info.id = uuidv4();
+            }
+
+            info.registeredAt = new Date();
+            info.lastHeartbeat = new Date();
+            this.nodes[info.id] = Object.assign({}, info);
+
+            res.send(info);
+        }
+    }, {
+        key: 'deregisterNode',
+        value: function deregisterNode(req, res) {
+            if (!res) {
+                this.nodes[req] = null;
+                delete this.nodes[req];
+            } else {
+                this.nodes[req.params.id] = null;
+                delete this.nodes[req.params.id];
+            }
+        }
+    }, {
+        key: 'initClient',
+        value: function initClient() {
+            this.connect();
+        }
+    }, {
+        key: 'connect',
+        value: function connect() {
+            var _this3 = this;
+
+            if (this.options.url) {
+                var url = this.options.url + this.ENDPOINTS.register;
+                var data = {
+                    id: this.options.id,
+                    ip: ip(),
+                    interval: this.options.interval,
+                    hostname: os.hostname()
+                };
+
+                request.post(url, { json: data }, function (e, r, body) {
+                    if (body && body.id) {
+                        _this3.options.id = body.id;
+                    }
+
+                    _this3.sendHeartbeat();
+                });
+            }
+        }
+    }, {
+        key: 'sendHeartbeat',
+        value: function sendHeartbeat() {
+            var _this4 = this;
+
+            var url = this.options.url + ('/nodes/' + this.options.id);
+            var data = {
+                ip: ip()
+            };
+
+            request.put(url, { json: data }, function (e, r, body) {
+                if (r && r.statusCode !== 404) {
+                    _this4.timeout = setTimeout(_this4.sendHeartbeat, _this4.options.interval);
+                } else {
+                    _this4.connect();
+                }
+            });
+        }
+    }, {
+        key: 'clearNode',
+        value: function clearNode(node, multiple) {
+            if (multiple === true) {
+                var nodes = {};
+                for (var n in node) {
+                    var _node$n = node[n],
+                        timeout = _node$n.timeout,
+                        ret = _objectWithoutProperties(_node$n, ['timeout']);
+
+                    nodes[n] = ret;
+                }
+                return nodes;
+            } else {
+                var timeout = node.timeout,
+                    ret = _objectWithoutProperties(node, ['timeout']);
+
+                return ret;
+            }
+        }
+    }, {
+        key: 'log',
+        value: function log() {
+            if (this.options.verbose !== false) {
+                for (var _len = arguments.length, attrs = Array(_len), _key = 0; _key < _len; _key++) {
+                    attrs[_key] = arguments[_key];
                 }
 
-                this.sendHeartbeat()
-            })
-        }
-    }
-
-    sendHeartbeat() {
-        let url = this.options.url + `/nodes/${this.options.id}`
-        let data = {
-            ip: ip()
-        }
-
-        request.put(url, { json: data }, (e, r, body) => {
-            if (r && r.statusCode !== 404) {
-                this.timeout = setTimeout(this.sendHeartbeat, this.options.interval)
+                console.log(attrs.join(', '));
             }
-            else {
-                this.connect()
-            }
-        })
-    }
+        }
+    }]);
 
-    clearNode(node, multiple) {
-        if (multiple === true) {
-            let nodes = {}
-            for(let n in node) {
-                let { timeout, ...ret } = node[n];
-                nodes[n] = ret;
-            }
-            return nodes
-        }
-        else {
-            let { timeout, ...ret } = node
-            return ret
-        }
-    }
-
-    log(...attrs) {
-        if (this.options.verbose !== false) {
-            console.log(attrs.join(', '))
-        }
-    }
-}
+    return Pulsatio;
+}();
 
 function ip() {
-    let interfaces = os.networkInterfaces()
-    for (let devName in interfaces) {
-        let iface = interfaces[devName]
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
 
-        for (let i = 0; i < iface.length; i++) {
-            let alias = iface[i]
-            let last_digit = alias.address.substring(alias.address.length - 2, alias.address.length)
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            var last_digit = alias.address.substring(alias.address.length - 2, alias.address.length);
             if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal && last_digit !== '.1') {
-                return alias.address
+                return alias.address;
             }
         }
     }
 
-    return '0.0.0.0'
+    return '0.0.0.0';
 }
 
-module.exports = Pulsatio
+module.exports = Pulsatio;
---
+++ src/index.js
@@ -0,0 +1,226 @@
+const request = require('request')
+const uuidv4 = require('uuid/v4')
+const os = require('os')
+
+class Pulsatio {
+    constructor(options = {}) {
+        let defaults = {
+            port: 4200,
+            url: 'http://localhost:4200',
+            interval: 30 * 1000,
+            interval_timeout: 1.1
+        }
+
+        options = Object.assign(defaults, options)
+
+        this.ENDPOINTS = {
+            register: '/nodes',
+            getAllNodes: '/nodes'
+        }
+
+        if (!options.express) {
+            var express = require('express')
+            this.express = express()
+        }
+        else {
+            this.express = options.express
+        }
+
+        this.nodes = {}
+        this.options = options
+
+        this.registerNewNode = this.registerNewNode.bind(this)
+        this.pulsatio = this.pulsatio.bind(this)
+        this.getNode = this.getNode.bind(this)
+        this.getAllNodes = this.getAllNodes.bind(this)
+        this.sendHeartbeat = this.sendHeartbeat.bind(this)
+        this.connect = this.connect.bind(this)
+
+        this.init()
+    }
+
+    init() {
+        if (this.options.mode === 'server') {
+            this.initServer()
+        }
+        else {
+            this.initClient()
+        }
+    }
+
+    initServer() {
+        let address
+        if (this.express.address) {
+            try {
+                address = this.express.address()
+            } catch (err) { }
+        }
+
+        var bodyParser = require('body-parser')
+        this.express.use(bodyParser.json())
+
+        if (!address) {
+            this.express.listen(this.options.port, () => {
+                this.log(`Pulsat.io @ ${this.options.port}`)
+            })
+        }
+
+        this.initServerEndpoints()
+    }
+
+    initServerEndpoints() {
+        this.express.put('/nodes/:id', this.pulsatio)
+        this.express.get('/nodes/:id', this.getNode)
+        this.express.get(this.ENDPOINTS.getAllNodes, this.getAllNodes)
+        this.express.post(this.ENDPOINTS.register, this.registerNewNode)
+        this.express.delete('/nodes/:id', this.deregisterNode)
+    }
+
+    pulsatio(req, res) {
+        let node = this.nodes[req.params.id]
+
+        if (node) {
+            clearTimeout(node.timeout)
+            this.nodes[req.params.id].online = true;
+            this.nodes[req.params.id].lastHeartbeat = new Date()
+            this.nodes[req.params.id].timeout = setTimeout(() => {
+                this.nodes[req.params.id].online = false
+            }, node.interval * this.options.interval_timeout)
+
+            res.sendStatus(200)
+        }
+        else {
+            res.sendStatus(404)
+        }
+    }
+
+    getNode(req, res) {
+        if (!res) {
+            return this.clearNode(this.nodes[req])
+        }
+        else {
+            res.send(this.clearNode(this.nodes[req.params.id]))
+        }
+    }
+
+    getAllNodes(req, res) {
+        if (!res) {
+            return this.clearNode(this.nodes, true)
+        }
+        else {
+            res.send(this.clearNode(this.nodes, true))
+        }
+    }
+
+    registerNewNode(req, res) {
+        let info = req.body
+        info.online = true
+
+        if (info.id && this.nodes[info.id]) {
+            res.send({ info: 'Already registered!' })
+            return
+        }
+        if (!info.id) {
+            info.id = uuidv4()
+        }
+
+        info.registeredAt = new Date()
+        info.lastHeartbeat = new Date()
+        this.nodes[info.id] = Object.assign({}, info)
+
+        res.send(info)
+    }
+
+    deregisterNode(req, res) {
+        if (!res) {
+            this.nodes[req] = null
+            delete this.nodes[req]
+        }
+        else {
+            this.nodes[req.params.id] = null
+            delete this.nodes[req.params.id]
+        }
+    }
+
+    initClient() {
+        this.connect()
+    }
+
+    connect() {
+        if (this.options.url) {
+            let url = this.options.url + this.ENDPOINTS.register
+            let data = {
+                id: this.options.id,
+                ip: ip(),
+                interval: this.options.interval,
+                hostname: os.hostname()
+            }
+
+            request.post(url, { json: data }, (e, r, body) => {
+                if (body && body.id) {
+                    this.options.id = body.id
+                }
+
+                this.sendHeartbeat()
+            })
+        }
+    }
+
+    sendHeartbeat() {
+        let url = this.options.url + `/nodes/${this.options.id}`
+        let data = {
+            ip: ip()
+        }
+
+        request.put(url, { json: data }, (e, r, body) => {
+            if (r && r.statusCode !== 404) {
+                this.timeout = setTimeout(this.sendHeartbeat, this.options.interval)
+            }
+            else {
+                this.connect()
+            }
+        })
+    }
+
+    clearNode(node, multiple) {
+        if (multiple === true) {
+            let nodes = {}
+            for(let n in node) {
+                let copy = Object.assign({}, node[n])
+                copy.timeout = null
+                delete copy.timeout
+                nodes[n] = copy
+            }
+            return nodes
+        }
+        else {
+            let { timeout, ...ret } = node
+            return ret
+        }
+    }
+
+    log(...attrs) {
+        if (this.options.verbose !== false) {
+            console.log(attrs.join(', '))
+        }
+    }
+}
+
+function ip() {
+    let interfaces = os.networkInterfaces()
+    for (let devName in interfaces) {
+        let iface = interfaces[devName]
+
+        for (let i = 0; i < iface.length; i++) {
+            let alias = iface[i]
+            let last_digit = alias.address.substring(alias.address.length - 2, alias.address.length)
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal && last_digit !== '.1') {
+                return alias.address
+            }
+        }
+    }
+
+    return '0.0.0.0'
+}
+
+module.exports = Pulsatio
