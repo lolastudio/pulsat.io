a6fdbd6bafcb594a6dd723a24a669f568eef86b7
Roque Francisco <roquefsd@gmail.com>
2018-12-28 14:06:26 -0200

    Middleware

--- test/server.js
+++ test/server.js
@@ -1,6 +1,11 @@
 var Pulsatio = require('../index.js');
-var ps = new Pulsatio({ mode: 'server' });
+var ps = new Pulsatio({ mode: 'server', on: {
+    connection: (res, next) => {
+        console.log('new connection');
+        next();
+    }
+} });
 
 setInterval(() => {
-    console.log(ps.getAllNodes());
+    // console.log(ps.getAllNodes());
 }, 5000);
